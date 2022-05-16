1859b26c78087e46943bbc94ffd81f8d299744d7
Javier <javirros123@gmail.com>
2022-05-16 13:37:54 +0200

    Fotos

--- src/modelos/App.java
+++ src/modelos/App.java
@@ -7,6 +7,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -20,8 +22,9 @@ public class App extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("../vistas/MenuPrincipal.fxml"));
             Scene scene = new Scene(root);
-
-
+            Image image1 = new Image("../../Documents/ProyectoPokemon/rsc/Pokemon-UK-1.jpg");
+            ImageView image2 = new ImageView("../../Documents/ProyectoPokemon/rsc/VIR-2713-62-cual-es-tu-nivel-d-unscreen.gif");
+           
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
